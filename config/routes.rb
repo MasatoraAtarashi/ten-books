d6c92264cbc16a39794e853b211db983f80ce625
あたらしまさとら <masatora.atarashi@gmail.com>
2019-08-02 02:54:18 +0900

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root "top#index"
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
-  #あとでRESTfulに直す
-  post 'users/:id', to: 'users#update'
+  post 'users/:id', to: 'users#update'#あとでRESTfulに直す
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
