bd6b92b5d501d79be6658f0b0e773373d93f788a
あたらしまさとら <masatora.atarashi@gmail.com>
2019-08-01 16:06:41 +0900

    make signup and display error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
-  def new
+ 
+  def show
+    @user = User.find(params[:id])
+  end
+
+   def new
     @user = User.new
   end
 
-  def show
-  end
+   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new' 
+    end
+   end
+
+   private
+    def user_params
+      p params
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
 end
