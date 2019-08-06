ecde3ac9d7061b24d4659c368c812cae2064345e
あたらしまさとら <masatora.atarashi@gmail.com>
2019-08-06 09:04:01 +0900

    Change books index amount

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
   # 検索ボックスの内容をもとに書籍を検索して表示する
   def show
     @keyword = params[:id]
-    @books = GoogleBooks.search(@keyword, {:count => 30}).to_a.paginate(page: params[:page], :per_page => 10)
+    @books = GoogleBooks.search(@keyword, {:count => 40}).to_a.paginate(page: params[:page], :per_page => 10)
   end
 
   # 本棚に本データを追加する(十冊を選ぶ)
