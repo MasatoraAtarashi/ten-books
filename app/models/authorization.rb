7647eaea39e51e71fd3bc09f33a53504f37015b0
あたらしまさとら <masatora.atarashi@gmail.com>
2019-08-12 12:37:29 +0900

    Modify few things

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -6,14 +6,14 @@ class Authorization < ApplicationRecord
   validates :uid, uniqueness: { scope: :provider }
 
 
-  def self.find_from_auth(auth)
-    find_by_provider_and_uid(auth['provider'], auth['uid'])
-  end
-
-  def self.create_from_auth(auth, user = nil)
-    user ||= User.create_from_auth!(auth)
-    p user
-    p auth
-    Authorization.create!(:user => user, :uid => auth['uid'], :provider => auth['provider'])
-  end
+  # def self.find_from_auth(auth)
+  #   find_by_provider_and_uid(auth['provider'], auth['uid'])
+  # end
+  #
+  # def self.create_from_auth(auth, user = nil)
+  #   user ||= User.create_from_auth!(auth)
+  #   p user
+  #   p auth
+  #   Authorization.create!(:user => user, :uid => auth['uid'], :provider => auth['provider'])
+  # end
 end
